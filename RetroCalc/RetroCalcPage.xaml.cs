7962c6340d1f3a417ea8c9219636a38721352d58
Mohit Athwani <mohit@athwani.net>
2017-09-20 18:38:41 -0700

    * .gitignore: Added First 2 Images and setting up the project
    * Main.cs:
    * RetroCalc.sln:
    * Info.plist:
    * AppDelegate.cs:
    * App.xaml:
    * packages.config:
    * MainActivity.cs:
    * packages.config:
    * App.xaml.cs:
    * Entitlements.plist:
    * RetroCalc.iOS.csproj:
    * packages.config:
    * RetroCalc.csproj:
    * LaunchScreen.storyboard:
    * ground.png:
    * RetroCalc.Droid.csproj:
    * RetroCalcPage.xaml:
    * AboutAssets.txt:
    * space-bg.png:
    * RetroCalcPage.xaml.cs:
    * AssemblyInfo.cs:
    * styles.xml:
    * icon.png:
    * Contents.json:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AssemblyInfo.cs:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ RetroCalc/RetroCalcPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace RetroCalc
+{
+    public partial class RetroCalcPage : ContentPage
+    {
+        public RetroCalcPage()
+        {
+            InitializeComponent();
+            mainBanckgroundImage.Source = ImageSource.FromResource("RetroCalc.Images.space-bg.png");
+            backPanelImage.Source = ImageSource.FromResource("RetroCalc.Images.ground.png");
+        }
+    }
+}
